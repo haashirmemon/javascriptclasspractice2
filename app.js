cb0bdfa1a50db429a0f13a509423b1313f7ddc17
haashirmemon <haashirmemon6@gmail.com>
2024-04-20 09:39:59 -0700

    file

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var uservalue = +prompt("enter your value")
-if (uservalue > 10) {
-    document.write(uservalue)
-} else{
-    document.write('garaq ho')
+// var uservalue = +prompt("enter your value")
+// if (uservalue > 10) {
+//     document.write(uservalue)
+// } else{
+//     document.write('garaq ho')
+// }
+var guess = prompt('enter bond number')
+// guess = Number(guess)
+// var secretnumber = 15
+// var lowprize = {min:6 ,max:14, }
+var lownumber = 14
+var highnumber = 100
+var mediumnumber = 50
+if (guess<=14) {
+    document.write('cogragulation you won lowprize')
+}else if (guess<=50) {
+    document.write('cogragulation you midium prize')
+} else if(guess<=100) {
+    document.write('cogragulation you high prize')
+} else {
+    document.write('garaq hoja')
 }
+
