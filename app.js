3e81f13540d41b06f6a4600e7d9e1ee10ffd2025
haashirmemon <haashirmemon6@gmail.com>
2024-04-26 01:24:25 -0700

    file

--- app.js
+++ app.js
@@ -17,55 +17,83 @@
 //     document.write('cogragulation you midium prize')
 // } else if(guess<=100) {
 //     document.write('cogragulation you high prize')
-// } else {
-//     document.write('garaq hoja')
-// }
-// Password Generator
-function generatePassword() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let password = '';
-    for (let i = 0; i < 10; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      password += characters.charAt(randomIndex);
-    }
-    console.log("Generated Password:", password);
-  }
+// // } else {
+// //     document.write('garaq hoja')
+// // }
+// // Password Generator
+// function generatePassword() {
+//     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//     let password = '';
+//     for (let i = 0; i < 10; i++) {
+//       const randomIndex = Math.floor(Math.random() * characters.length);
+//       password += characters.charAt(randomIndex);
+//     }
+//     console.log("Generated Password:", password);
+//   }
   
-  // Word Reverser
-  function reverseWords(sentence) {
-    const words = sentence.split(' ');
-    const reversedSentence = words.reverse().join(' ');
-    console.log("Reversed Sentence:", reversedSentence);
-  }
+//   // Word Reverser
+//   function reverseWords(sentence) {
+//     const words = sentence.split(' ');
+//     const reversedSentence = words.reverse().join(' ');
+//     console.log("Reversed Sentence:", reversedSentence);
+//   }
   
-  // Word Capitalizer
-  function capitalizeWords(sentence) {
-    const words = sentence.split(' ');
-    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-    const capitalizedSentence = capitalizedWords.join(' ');
-    console.log("Capitalized Sentence:", capitalizedSentence);
-  }
+//   // Word Capitalizer
+//   function capitalizeWords(sentence) {
+//     const words = sentence.split(' ');
+//     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     const capitalizedSentence = capitalizedWords.join(' ');
+//     console.log("Capitalized Sentence:", capitalizedSentence);
+//   }
   
-  // Word Finder
-  function findWord(sentence, word) {
-    if (sentence.includes(word)) {
-      console.log("Word found");
-    } else {
-      console.log("Word not found");
-    }
-  }
+//   // Word Finder
+//   function findWord(sentence, word) {
+//     if (sentence.includes(word)) {
+//       console.log("Word found");
+//     } else {
+//       console.log("Word not found");
+//     }
+//   }
   
-  // String Concatenation
-  function concatenateNames(firstName, lastName) {
-    const fullName = `${firstName} ${lastName}`;
-    console.log("Full Name:", fullName);
-  }
+//   // String Concatenation
+//   function concatenateNames(firstName, lastName) {
+//     const fullName = `${firstName} ${lastName}`;
+//     console.log("Full Name:", fullName);
+//   }
   
-  // Test cases
-  generatePassword();
-  reverseWords("This is a sample sentence");
-  capitalizeWords("this is a capitalized sentence");
-  findWord("Hello world", "world");
-  concatenateNames("John", "Doe");
-  
+//   // Test cases
+//   generatePassword();
+//   reverseWords("This is a sample sentence");
+//   capitalizeWords("this is a capitalized sentence");
+//   findWord("Hello world", "world");
+//   concatenateNames("John", "Doe");
+//   var input1 = document.getElementsByClassName('input')
+//   var input2 = document.getElementsByClassName('input')
+//   var input3 = document.getElementsByClassName('input')
+//   var div = document.getElementById('div1')
+// var div.style.backgroundcolor=
+//   function applycolor(color) {
+//     input1.style.backgroundcolor =("green")
+//     input2.style.backgroundcolor =("red")
+//     input3.style.backgroundcolor =("yellow")
+
+//   }
+//   changecolor(color)
+// Apply the color to the div
+function applyColor(color) {
+  // event.preventDefault(); // Prevent form submission
+  var red = parseInt(document.getElementById("red").value);
+  var green = parseInt(document.getElementById("green").value);
+  var blue = parseInt(document.getElementById("blue").value);
+
+
+
+
+  var div1= document.getElementById("div1");
+  div1.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+}
+
+
+ applyColor(color) 
 
